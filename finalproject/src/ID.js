04b3a82f857be289a64ba648571eef7ca90f695b
arjunmahendra1 <159055747+arjunmahendra1@users.noreply.github.com>
2024-04-28 16:00:49 -0400

    Update ID.js

--- finalproject/src/ID.js
+++ finalproject/src/ID.js
@@ -23,3 +23,31 @@ function ID ({person}) {
 }
 
 export default ID
+
+//
+
+import React from 'react';
+import './ID.css';
+
+function ID({ person, index }) {
+  // Determine the side of the frame based on the index
+  const alignment = index % 2 === 0 ? 'left' : 'right';
+
+  // You can add more styling conditions based on the alignment if needed
+  const style = {
+    backgroundColor: person.backgroundColor,
+    alignSelf: alignment === 'left' ? 'flex-start' : 'flex-end',
+  };
+
+  return (
+    <div style={style} className={`profile ${alignment}`}>
+      <img className="profile-image" src={person.image_src} alt={person.name} />
+      <div className="profile-info">
+        <p><b>Name:</b> {person.name}</p>
+        <p><b>Role:</b> {person.role}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ID;
