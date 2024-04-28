2ab57eff992269b00b4a941a95c9c78576b19fbc
Rivan Parikh <rivan.parikh@gmail.com>
2024-04-28 18:38:46 -0400

    new id.js

--- finalproject/src/ID.js
+++ finalproject/src/ID.js
@@ -1,31 +1,3 @@
-import './ID.css'
-
-function ID ({person}) {
-
-    const style = {
-        backgroundColor: person.backgroundColor,
-        flexDirection: person.direction === 'left' ? 'row' : 'row-reverse',
-    };
-
-    return(
-
-        <div style={style} class="container">
-            <img id="image" src={person.image_src} alt={person.name}/>
-
-            <div>
-                <p><b>Name:</b> {person.name}</p>
-                <p><b>Role:</b> {person.role}</p>
-            </div>
-
-        </div>
-
-    );
-}
-
-export default ID
-
-----------------------------modified code---------------------------------------
-
 import './ID.css';
 
 function ID({ person, index }) {
